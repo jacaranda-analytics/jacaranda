bc9c935563c908107af63a5dadd5e053bea3e7fd
Harry Goodman <harryfgoodman@gmail.com>
2021-10-23 01:04:39 +1000

    feat: vae, autoencode are now in general Also update examples to accomadate changes

--- examples/jacaranda-cnn1d.py
+++ examples/jacaranda-cnn1d.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 import jacaranda.pytorch as jac_torch
 
 # Genereate 50 realisations with 10 covariates
@@ -8,7 +9,15 @@ Y = np.random.randint(5, size=(50, 1)).astype(dtype=np.float32)
 # Expand dimensions so that we can use a cnn
 X = np.expand_dims(X, axis=1)
 
-config = jac_torch.pytorch_config(X, Y, n_trials=2)
+def loss_function(x, model, target):
+    output = model(x)
+    loss = torch.nn.MSELoss()
+    return loss(output, target)
+
+def metric_function(x, model, target):
+    return loss_function(x, model, target).item()
+
+config = jac_torch.pytorch_config(X, Y, n_trials=2, loss = loss_function, metric= metric_function)
 
 cnn = jac_torch.pytorch_general(X, Y, config, define_model=jac_torch.pytorch_cnn1d)
 cnn.tune()
--- jacaranda/pytorch.py
+++ jacaranda/pytorch.py
@@ -4,6 +4,7 @@ import os
 import torch.nn as nn
 import torch
 import torch.optim as optim
+import torch.nn.functional as F
 from torch.utils.data.sampler import SubsetRandomSampler
 import optuna
 from optuna.trial import TrialState
@@ -166,7 +167,6 @@ class pytorch_general:
         lr = trial.suggest_float("lr", 1e-5, 1e-3, log=True)
         optimizer = getattr(optim, optimizer_name)(model.parameters(), lr=lr)
 
-        # Get the FashionMNIST dataset.
         train_loader, valid_loader = self.data_loader()
 
         # Training of the model.
@@ -176,8 +176,8 @@ class pytorch_general:
                 data, target = train_data
                 # target = target.unsqueeze(1)
                 optimizer.zero_grad()
-                output = model(data)
-                loss = loss_function(output, target)
+                # output = model(data)
+                loss = loss_function(data, model ,target)
                 loss.backward()
                 optimizer.step()
 
@@ -188,8 +188,9 @@ class pytorch_general:
             for val_data in valid_loader:
                 X_val, y_true = val_data
 
-            y_pred = model(X_val).detach().numpy()
-            accuracy = self.config.METRIC(y_true, y_pred)
+            # y_pred = model(X_val).detach().numpy()
+            
+            accuracy = self.config.METRIC(X_val, model, y_true)
 
             trial.report(accuracy, epoch)
 
@@ -229,8 +230,8 @@ class pytorch_general:
                 data, target = train_data
                 # target = target.unsqueeze(1)
                 optimizer.zero_grad()
-                output = model(data)
-                loss = loss_function(output, target)
+                # output = model(data)
+                loss = loss_function(data, model, target)
                 loss.backward()
                 optimizer.step()
 
@@ -366,207 +367,60 @@ class pytorch_cnn1d(nn.Module):
         return x
 
 
-class pytorch_autoencoder:
-    """This is a class that tunes an autoencoder for given data"""
+class pytorch_autoencoder(nn.Module):
+    """General autoencoder class to tune"""
 
-    def __init__(self, X: numpy.array, config: typing.Type[pytorch_config]):
-        """[summary]
-
+    def __init__(self, config, trial):
+        """
         Args:
-            X (numpy.array): Independent Covariates
-            config (typing.Type[pytorch_config]):config class specific this is handled by pytorch_general
+            X (numpy.array): Independent Covariates (handled by outside class)
+            encode_level (float): Encode reduction, proportion of variables to reduce to.
+            Must be positive (handled by outside class)
         """
-        self.X = X
-        self.Y = X
-        self.config = config
-        self.model = "Model has not been tuned yet"
-        self.model_paramaters = "Model has not been tuned yet"
-        self.model_metric = "Model has not been tuned yet"
-
-    def data_loader(self):
-
-        dataset = [[self.X[i], self.Y[i]] for i in range(len(self.X))]
-
-        # Creating data indices for training and validation splits:
-        dataset_size = len(dataset)
-        indices = list(range(dataset_size))
-        split = int(numpy.floor(self.config.VALIDATION_SPLIT * dataset_size))
-        numpy.random.seed(self.config.SEED)
-        numpy.random.shuffle(indices)
-        train_indices, val_indices = indices[split:], indices[:split]
-
-        # Creating PT data samplers and loaders:
-        train_sampler = SubsetRandomSampler(train_indices)
-        valid_sampler = SubsetRandomSampler(val_indices)
-
-        train_loader = torch.utils.data.DataLoader(
-            dataset, batch_size=self.config.BATCHSIZE, sampler=train_sampler
+        super(pytorch_autoencoder, self).__init__()
+        self.trial = trial
+        self.encoder = nn.Sequential(
+            nn.Linear(config.IN_FEATURES_START, config.ENCODE_LEVEL), nn.ReLU()
         )
-        validation_loader = torch.utils.data.DataLoader(
-            dataset, batch_size=len(valid_sampler), sampler=valid_sampler
-        )
+        self.decoder = nn.Linear(config.ENCODE_LEVEL, config.IN_FEATURES_START)
 
-        return train_loader, validation_loader
+    def forward(self, x):
+        latent = self.encoder(x)
+        decoded = self.decoder(latent)
+        decoded = torch.sigmoid(decoded)
+        return decoded, latent
 
-    def objective(self, trial):
-        """Objective function for Optuna tune.
+class pytorch_vae(nn.Module):
+    def __init__(self, config, trial):
+        super(pytorch_vae, self).__init__()
 
-        Args:
-            trial: Optuna trial
-        Raises:
-            optuna.exceptions.TrialPruned: Optuna inbuilt pruning
+        image_size= config.IN_FEATURES_START
+        h_dim= 20
+        z_dim= 10
 
-        Returns:
-            float: accuracy of current model, defined by config.METRIC
-        """
+        self.trial = trial
 
-        loss_function = self.config.LOSS
+        self.fc1 = nn.Linear(image_size, h_dim)
+        self.fc2 = nn.Linear(h_dim, z_dim)
+        self.fc3 = nn.Linear(h_dim, z_dim)
+        self.fc4 = nn.Linear(z_dim, h_dim)
+        self.fc5 = nn.Linear(h_dim, image_size)
+        
+    def encode(self, x):
+        h = F.relu(self.fc1(x))
+        return self.fc2(h), self.fc3(h)
 
-        # Generate the model.'
-        layers = []
-        in_features = self.config.IN_FEATURES_START
-        out_features = self.config.ENCODE_LEVEL
-        layers.append(nn.Linear(in_features, out_features))
-        layers.append(nn.ReLU())
-        in_features = out_features
-        layers.append(nn.Linear(in_features, self.config.CLASSES))
-        model = nn.Sequential(*layers).to(self.config.DEVICE)
+    def reparameterize(self, mu, log_var):
+        std = torch.exp(log_var/2)
+        eps = torch.randn_like(std)
+        return mu + eps * std
 
-        # model = self.define_model(trial).to(self.config.DEVICE)
+    def decode(self, z):
+        h = F.relu(self.fc4(z))
+        return torch.sigmoid(self.fc5(h))
 
-        # Generate the optimizers.
-        optimizer_name = trial.suggest_categorical(
-            "optimizer", ["Adam", "RMSprop", "SGD"]
-        )
-        lr = trial.suggest_float("lr", 1e-5, 1e-3, log=True)
-        optimizer = getattr(optim, optimizer_name)(model.parameters(), lr=lr)
-
-        # Get the FashionMNIST dataset.
-        train_loader, valid_loader = self.data_loader()
-
-        # Training of the model.
-        for epoch in range(self.config.EPOCHS):
-            model.train()
-            for train_data in train_loader:
-                data, target = train_data
-                # target = target.unsqueeze(1)
-                optimizer.zero_grad()
-                output = model(data)
-                loss = loss_function(output, target)
-                loss.backward()
-                optimizer.step()
-
-            model.eval()
-            # need to fix, this
-            for val_data in valid_loader:
-                X_val, y_true = val_data
-
-            y_pred = model(X_val).detach().numpy()
-            accuracy = self.config.METRIC(y_true, y_pred)
-
-            trial.report(accuracy, epoch)
-
-            # Handle pruning based on the intermediate value.
-            if trial.should_prune():
-                raise optuna.exceptions.TrialPruned()
-
-        return accuracy
-
-    def build_model(self, study):
-        """Rebuilds best model from Optuna search
-
-        Args:
-            study ([type]): Best study from Optuna search
-
-        Returns:
-            [type]: best model
-        """
-
-        class autoencoder_final(nn.Module):
-            """General autoencoder class to tune"""
-
-            def __init__(self, X, encode_level):
-                """
-                Args:
-                    X (numpy.array): Independent Covariates (handled by outside class)
-                    encode_level (float): Encode reduction, proportion of variables to reduce to.
-                    Must be positive (handled by outside class)
-                """
-                super(autoencoder_final, self).__init__()
-                self.encoder = nn.Sequential(
-                    nn.Linear(X.shape[1], encode_level), nn.ReLU()
-                )
-                self.decoder = nn.Linear(encode_level, X.shape[1])
-
-            def forward(self, x):
-                latent = self.encoder(x)
-                decoded = self.decoder(latent)
-                decoded = torch.sigmoid(decoded)
-                return decoded, latent
-
-        model = autoencoder_final(self.X, self.config.ENCODE_LEVEL).to(
-            self.config.DEVICE
-        )
-
-        # Data
-        train_loader, _ = self.data_loader()
-
-        # Loss
-        loss_function = nn.MSELoss()
-
-        # Optimiser
-        optimizer_name = study.best_trial.params["optimizer"]
-        lr = study.best_trial.params["lr"]
-        optimizer = getattr(optim, optimizer_name)(model.parameters(), lr=lr)
-
-        # Training of the model.
-        for _ in range(self.config.EPOCHS):
-            model.train()
-
-            for train_data in train_loader:
-                data, target = train_data
-                # target = target.unsqueeze(1)
-
-                optimizer.zero_grad()
-                output, _ = model(data)
-                loss = loss_function(output, target)
-                loss.backward()
-                optimizer.step()
-
-        return model
-
-    def tune(self):
-        """Engages the Optuna search
-
-        Returns:
-            [type]: best model
-        """
-        study = optuna.create_study(direction="maximize")
-        study.optimize(
-            self.objective, n_trials=self.config.N_TRIALS, timeout=self.config.TIMEOUT
-        )
-
-        pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
-        complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
-
-        print("Study statistics: ")
-        print("  Number of finished trials: ", len(study.trials))
-        print("  Number of pruned trials: ", len(pruned_trials))
-        print("  Number of complete trials: ", len(complete_trials))
-
-        print("Best trial:")
-        trial = study.best_trial
-
-        print("  Value: ", trial.value)
-
-        print("  Params: ")
-        for key, value in trial.params.items():
-            print("    {}: {}".format(key, value))
-
-        model = self.build_model(study)
-
-        self.model_metric = trial.value
-        self.model_paramaters = trial.params
-        self.model = model
-
-        return model
+    def forward(self, x):
+        mu, log_var = self.encode(x)
+        z = self.reparameterize(mu, log_var)
+        x_reconst = self.decode(z)
+        return x_reconst, mu, log_var
